0d1da65591a8a9874f6c5297a4cd81b9df36e9d8
SeMsei <fomichevsjiy@gmail.com>
2023-10-05 18:06:35 +0300

    added entities

---
+++ ListUser/Domain/Models/ListModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models;
+
+public class ListModel<T>
+{
+	public List<T> Items { get; set; } = new();
+	public int CurrentPage { get; set; } = 1;
+	public int TotalPages { get; set; } = 1;
+}
+
+
+
+}

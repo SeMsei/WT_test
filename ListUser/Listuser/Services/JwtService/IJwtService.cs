926310df64605c0105193b0a71eed5ed025289d5
SeMsei <fomichevsjiy@gmail.com>
2023-10-10 16:52:25 +0300

    added jwt service

---
+++ ListUser/Listuser/Services/JwtService/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace ListUser.Services.JwtService;
+
+public interface IJwtService
+{
+	public string GenerateToken(string userId);
+	public ClaimsPrincipal ValidateToken(string token);
+
+}
---
+++ ListUser/Listuser/Services/JwtService/JwtService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ListUser.Services.JwtService;
+public class JwtService: IJwtService
+{
+	private readonly string _secretKey;
+	private readonly string _issuer;
+
+	public JwtService(string secretKey, string issuer)
+	{
+		_secretKey = secretKey;
+		_issuer = issuer;
+	}
+
+	public string GenerateToken(string userId)
+	{
+		var tokenHandler = new JwtSecurityTokenHandler();
+		var key = Encoding.ASCII.GetBytes(_secretKey);
+
+		var claims = new ClaimsIdentity(new[]
+		{
+			new Claim(ClaimTypes.Name, userId),
+		});
+
+		var tokenDescriptor = new SecurityTokenDescriptor
+		{
+			Subject = claims,
+			Expires = DateTime.UtcNow.AddHours(1), // Устанавливает срок действия токена
+			SigningCredentials = new SigningCredentials(
+				new SymmetricSecurityKey(key),
+				SecurityAlgorithms.HmacSha256Signature
+			),
+			Issuer = _issuer,
+			Audience = _issuer
+		};
+
+		var token = tokenHandler.CreateToken(tokenDescriptor);
+		return tokenHandler.WriteToken(token);
+	}
+
+	public ClaimsPrincipal ValidateToken(string token)
+	{
+		var tokenHandler = new JwtSecurityTokenHandler();
+		var key = Encoding.ASCII.GetBytes(_secretKey);
+
+		var validationParameters = new TokenValidationParameters
+		{
+			ValidateIssuer = true,
+			ValidateAudience = true,
+			ValidateLifetime = true,
+			ValidateIssuerSigningKey = true,
+			ValidIssuer = _issuer,
+			ValidAudience = _issuer,
+			IssuerSigningKey = new SymmetricSecurityKey(key),
+		};
+
+		try
+		{
+			var principal = tokenHandler.ValidateToken(token, validationParameters, out var _);
+			return principal;
+		}
+		catch (Exception)
+		{
+			// Токен недействителен или истек
+			return null;
+		}
+	}
+}

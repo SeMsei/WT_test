30d3d408eb8a88180fabd77bd9fd25ba57620cfc
SeMsei <fomichevsjiy@gmail.com>
2023-10-10 16:42:13 +0300

    added util to validate user information

---
+++ ListUser/Listuser/Services/Util/Validator.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+using ListUser.Services.UserService;
+
+namespace ListUser.Services.Util;
+
+public class UserValidator
+{
+	IUserService _userService;
+	public UserValidator(IUserService userService)
+	{
+		_userService = userService;
+	}
+
+	public async Task<ValidatorResponse> Validate(User? user, bool validateEmail = true)
+	{
+		if (user == null)
+		{
+			return new ValidatorResponse { Status = false, Message = "User is null"};
+		}
+
+		if (user.Email == null 
+			|| user.Age == null 
+			|| user.Name == null)
+		{
+			return new ValidatorResponse { Status = false, Message = "One of required field is empty" };
+		}
+
+		if (user.Age < 0)
+		{
+			return new ValidatorResponse { Status = false, Message = "Incorrect age" };
+		}
+		if (validateEmail) 
+		{ 
+			if (!(await _userService.IsEmailUnique(user.Email)))
+			{
+				return new ValidatorResponse { Status = false, Message = "Not unique email" };
+			}
+		}
+		
+
+		return new ValidatorResponse { Status = true, Message = "OK" }; ;
+	}
+}

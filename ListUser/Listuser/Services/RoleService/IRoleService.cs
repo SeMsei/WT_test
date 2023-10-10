3ff99cde3984b6be7bd88694520c820b7d0dfc55
SeMsei <fomichevsjiy@gmail.com>
2023-10-10 16:46:17 +0300

    added role service

---
+++ ListUser/Listuser/Services/RoleService/IRoleService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Domain.Models;
+
+namespace ListUser.Services.RoleService;
+
+public interface IRoleService
+{
+	public Task AddUser(int id, int userId);
+	public Task<bool> DoesRoleExistAsync(int id);
+}
---
+++ ListUser/Listuser/Services/RoleService/RoleService.cs
@@ -0,0 +1,32 @@
+﻿using ListUser.Data;
+using Domain.Entities;
+using Domain.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListUser.Services.RoleService;
+
+public class RoleService: IRoleService
+{
+	private AppDbContext _context;
+	public RoleService(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task AddUser(int id, int userId)
+	{
+		var role = await _context.Roles.FindAsync(id);
+		var user = await _context.Users.FindAsync(userId);
+		role.Users.Add(user);
+		//role.Users.Append(user);
+
+		await _context.SaveChangesAsync();
+	}
+
+	public async Task<bool> DoesRoleExistAsync(int id)
+	{
+		return await _context.Roles.AnyAsync(u => u.Id == id);
+	}
+}

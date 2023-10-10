552295bdde6e3e1b25ce02bd664e113d896dbac5
SeMsei <fomichevsjiy@gmail.com>
2023-10-10 16:22:26 +0300

    Updated Entities

--- ListUser/Domain/Entities/Role.cs
+++ ListUser/Domain/Entities/Role.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain.Entities;
@@ -10,4 +11,6 @@ public class Role
 {
 	public int Id { get; set; }
 	public string Name { get; set; } = string.Empty;
+	[JsonIgnore]
+	public ICollection<User> ?Users { get; set; }
 }
--- ListUser/Domain/Entities/User.cs
+++ ListUser/Domain/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,18 @@ namespace Domain.Entities;
 
 public class User
 {
+	[Required]
 	public int Id { get; set; }
+	[Required]
 	public string Name { get; set; } = string.Empty;
+	[Required]
 	public int Age { get; set; }
+	[Required]
+	[EmailAddress]
 	public string Email { get; set; } = string.Empty;
-	public IEnumerable<Role> ?roles { get; set; }
+	[Required]
+	public string Password { get; set; } = string.Empty;
+	[Required]
+	public ICollection<Role> ?Roles { get; set; }
 
 }
